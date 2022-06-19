9c3b8ab23fdc0e25b3b96c7452e646e4f165bd2d
Sudip Kundu <sudipkundu999@gmail.com>
2022-06-19 13:23:45 +0530

    style: feature heading bold

--- pages/projects.js
+++ pages/projects.js
@@ -5,7 +5,7 @@ import { Layout } from "../components/PageLayout";
 const projectsDB = [
   {
     name: "CobraStore",
-    description: "CobraStore is an eCommerce for book lovers",
+    description: "CobraStore is an eCommerce app for book lovers",
     tech: [
       {
         name: "ReactJS",
@@ -35,7 +35,7 @@ const projectsDB = [
   },
   {
     name: "CobraTube",
-    description: "CobraTube is an video library inspired by YouTube",
+    description: "CobraTube is an video library app inspired by YouTube",
     tech: [
       {
         name: "ReactJS",
@@ -65,7 +65,8 @@ const projectsDB = [
   },
   {
     name: "CobraTabs",
-    description: "CobraTabs is a chrome extension inspired by Momentum",
+    description:
+      "CobraTabs is a chrome extension inspired by Momentum to personalize new tabs",
     tech: [
       {
         name: "ReactJS",
@@ -153,16 +154,17 @@ const ProjectCard = ({ project }) => {
         ))}
       </h3>
       <article>
-        {description}. Core features are {features}.
+        {description}. <br />
+        <span className="font-semibold">Features</span> : {features}.
       </article>
       <span className="flex flex-row gap-4 m-auto my-1.5">
         <a href={github} target="_blank" rel="noopener noreferrer">
-          <button className="text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-semibold rounded-full text-base px-3 py-1 text-center mr-2 mb-2 dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-600 dark:focus:ring-cyan-800">
+          <button className="text-cyan-700 hover:text-white border border-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:outline-none focus:ring-cyan-300 font-semibold rounded-full text-base px-3 py-1 text-center mr-2 mb-2 dark:border-cyan-500 dark:text-cyan-500 dark:hover:text-white dark:hover:bg-cyan-600 dark:focus:ring-cyan-800 transition-colors duration-300">
             View Source
           </button>
         </a>
         <a href={live} target="_blank" rel="noopener noreferrer">
-          <button className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-semibold rounded-full text-base px-3 py-1 text-center mr-2 mb-2">
+          <button className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-semibold rounded-full text-base px-3 py-1 text-center mr-2 mb-2 transition-colors duration-300">
             Live Project
           </button>
         </a>
