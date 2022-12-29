d001f524e3849a7ad5db80cfbe288ed6e629b2f7
Sudip Kundu <sudipkundu999@gmail.com>
2022-12-29 22:29:31 +0530

    update description

--- pages/index.js
+++ pages/index.js
@@ -20,14 +20,11 @@ export default function Home() {
         <span className="text-cyan-600"> A Web Developer </span>
       </div>
       <article className="text-lg font-sans md:px-12 sm:px-8 px-2 text-center my-2">
-        I graduated in 2021 with a bachelor's in Civil Engineering from Manipal
-        Institute of Technology. After that, I was working as a Programmer
-        Analyst and also exploring different tech domains, I wanted to make a
-        portfolio website of my own back in December 2021 and that's when I
-        happen to fumble upon web development, Since then I really liked it, and
-        after giving a month into web dev, I finally decided to pursue it
-        full-time, I also got myself enrolled in a web dev bootcamp called
-        neogCamp, since then I've been learning and building projects.
+        I'm working as a UI Engineer @ Flipkart on the Partner network team.
+        Currently we are revamping the pages & adding new product features. In
+        my free time, I like to explore new technologies & building projects on
+        them. Currently, I'm learning data structures & algorithms and reading
+        about core CS fundamentals.
       </article>
       <h2 className="font-medium text-3xl text-center mt-10">My Skills</h2>
       <div className="flex flex-wrap gap-8 justify-center items-center my-6 max-w-lg mx-auto">
