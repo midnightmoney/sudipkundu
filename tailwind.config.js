ca5b4a0bd2ff2426397855e88f0b458a1f12c610
Sudip Kundu <sudipkundu999@gmail.com>
2023-01-16 21:04:48 +0530

    purge tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        pacifico: ["Pacifico"],
-        montserrat: ["Montserrat"],
+        pacifico: ['Pacifico'],
+        montserrat: ['Montserrat'],
       },
       screens: {
-        "550px": "550px",
-        "350px": "350px",
-        "666px": "666px",
+        '550px': '550px',
+        '350px': '350px',
+        '666px': '666px',
       },
-      minHeight: { main: "calc(100vh - 8.2rem)" },
+      minHeight: { main: 'calc(100vh - 8.2rem)' },
     },
   },
   plugins: [],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 };
